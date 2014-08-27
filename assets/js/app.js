8bf617e094cbc2c2b9967d0c8c0040eda080adf3
Stephanie Claudino Daffara <ste.daff@gmail.com>
2014-08-27 02:10:50 -0700

    added line

--- assets/js/app.js
+++ assets/js/app.js
@@ -82,7 +82,6 @@
 		        activeGroup = canvas.getActiveGroup();
 		    if (activeObject) {
 		      canvas.remove(activeObject);
-		      $("#text-string").val("");
 		    }
 		    else if (activeGroup) {
 		      var objectsInGroup = activeGroup.getObjects();
@@ -174,11 +173,9 @@
   }
 
   function addLine() {
-    var line = new fabric.Line({
-      fill: 'red',
+    var line = new fabric.Line([50, 100, 200, 200], {
       stroke: 'red',
       strokeWidth: 5,
-      selectable: false,
       left: 50,
       top: 50
 
