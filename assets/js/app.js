db03c9915833b5e0cd49d85beba0de3113b7c622
Stephanie Claudino Daffara <ste.daff@gmail.com>
2014-08-27 00:59:40 -0700

    added edit buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,9 +42,88 @@
 
 
   // create a wrapper around native canvas element (with id="c")
-  var canvas = new fabric.Canvas('c');
+  var canvas = new fabric.Canvas('c',{
+    hoverCursor: 'pointer',
+  });
+
+  // customize target when moving
+  canvas.on({
+    'object:moving': function(e) {
+      e.target.opacity = 0.6;
+    },
+    'object:modified': function(e) {
+      e.target.opacity = 1;
+    }
+  });
+
+  // find object that is being selected
+  canvas.findTarget = (function(originalFn) {
+	  return function() {
+	    var target = originalFn.apply(this, arguments);
+	    if (target) {
+	      if (this._hoveredTarget !== target) {
+	    	  canvas.fire('object:over', { target: target });
+	        if (this._hoveredTarget) {
+	        	canvas.fire('object:out', { target: this._hoveredTarget });
+	        }
+	        this._hoveredTarget = target;
+	      }
+	    }
+	    else if (this._hoveredTarget) {
+	    	canvas.fire('object:out', { target: this._hoveredTarget });
+	      this._hoveredTarget = null;
+	    }
+	    return target;
+	  };
+	})(canvas.findTarget);
+
+  document.getElementById('remove-selected').onclick = function() {
+		    var activeObject = canvas.getActiveObject(),
+		        activeGroup = canvas.getActiveGroup();
+		    if (activeObject) {
+		      canvas.remove(activeObject);
+		      $("#text-string").val("");
+		    }
+		    else if (activeGroup) {
+		      var objectsInGroup = activeGroup.getObjects();
+		      canvas.discardActiveGroup();
+		      objectsInGroup.forEach(function(object) {
+		        canvas.remove(object);
+		      });
+		    }
+	  };
+	  document.getElementById('bring-to-front').onclick = function() {
+		    var activeObject = canvas.getActiveObject(),
+		        activeGroup = canvas.getActiveGroup();
+		    if (activeObject) {
+		      activeObject.bringToFront();
+		    }
+		    else if (activeGroup) {
+		      var objectsInGroup = activeGroup.getObjects();
+		      canvas.discardActiveGroup();
+		      objectsInGroup.forEach(function(object) {
+		        object.bringToFront();
+		      });
+		    }
+	  };
+	  document.getElementById('send-to-back').onclick = function() {
+		    var activeObject = canvas.getActiveObject(),
+		        activeGroup = canvas.getActiveGroup();
+		    if (activeObject) {
+		      activeObject.sendToBack();
+		    }
+		    else if (activeGroup) {
+		      var objectsInGroup = activeGroup.getObjects();
+		      canvas.discardActiveGroup();
+		      objectsInGroup.forEach(function(object) {
+		        object.sendToBack();
+		      });
+		    }
+	  };
+
+
   canvas.setHeight(500);
-  canvas.setWidth(500);
+  canvas.setWidth(700);
 
   function addRect() {
     // create a rectangle object
@@ -58,22 +137,6 @@
 
     // "add" rectangle onto canvas
     canvas.add(rect);
-  }
-
-
-  function addDiamond() {
-    // create a rectangle with angle=45
-    var rect = new fabric.Rect({
-      left: 100,
-      top: 100,
-      fill: 'red',
-      width: 20,
-      height: 20,
-      angle: 45
-    });
-
-    canvas.add(rect);
-
   }
 
 
